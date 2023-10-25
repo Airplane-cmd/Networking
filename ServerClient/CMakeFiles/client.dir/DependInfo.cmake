769059b0956c75fb5847e229c809bfeb71c3b884
Sapsay Nikita <simonpatrikoff1@outlook.com>
2023-10-25 14:21:21 +1000

    yeah, fucked up

---
+++ ServerClient/CMakeFiles/client.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/bthd/WS/Javidx9/PixelGameEngine/BiggerProjects/Networking/Parts1&2/SimpleClient.cpp" "CMakeFiles/client.dir/SimpleClient.cpp.o" "gcc" "CMakeFiles/client.dir/SimpleClient.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
